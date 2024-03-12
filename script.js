bf8209a4a628397a15655b9cbc474d55d313477e
Four Pixels <121856163+4ourpixels@users.noreply.github.com>
2024-03-12 21:02:08 +0300

    Second commit

--- script.js
+++ script.js
@@ -29,3 +29,12 @@ nextBtn.addEventListener("click", (e)=>{
     }
     changeSlide();
 })
+
+const scrollContainer = document.querySelectorAll(".products");
+
+for(const item of scrollContainer){
+    item.addEventListener("wheel", (event)=>{
+        event.preventDefault();
+        item.scrollLeft += event.deltaY;
+    })
+}
